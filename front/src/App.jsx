c6975e818f3fe971742fa9bc76a69ffbb9efcd78
kcma1225 <157051689+kcma1225@users.noreply.github.com>
2025-04-02 20:13:24 +0800

    First Commit

---
+++ front/src/App.jsx
@@ -0,0 +1,21 @@
+// frontend/src/App.js
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [msg, setMsg] = useState('');
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/hello")
+      .then((res) => res.json())
+      .then((data) => setMsg(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>React + Flask Test</h1>
+      <p>{msg}</p>
+    </div>
+  );
+}
+
+export default App;
